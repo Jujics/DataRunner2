08878419de97cb3bb7acaa8ee2cd5da9185ea8a4
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-01-30 11:10:30 +0100

    boost work

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,30 +3,56 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("PlayerStats")]
-    [SerializeField]
+    [Header("Player Stats")]
     private float playerSpeed;
+    [SerializeField]
+    private float normalSpeed;
     [SerializeField]
     private float boostSpeed;
     [SerializeField]
     private float turnSpeed;
     [SerializeField]
     private float driftTurnSpeed;
+
     private Rigidbody rb;
-    
-    [Header("Movement")]
     private PlayerInput playerInput;
-    private InputActionMap forwardAction;
-    
+    private InputAction moveAction;
+    private InputAction boostAction;
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
-        forwardAction = playerInput.actions.FindActionMap("forward");
+        moveAction = playerInput.actions.FindAction("Forward");
+        boostAction = playerInput.actions.FindAction("Boost");
     }
 
     void FixedUpdate()
     {
-        Vector2 forward = forwardAction.ReadValue<Vector2>();
+        Boost();
+        Forward();
+    }
+
+    private void Boost()
+    {
+        float boostInput = boostAction.ReadValue<float>();
+        if (boostInput > 0)
+        {
+            playerSpeed = boostSpeed;
+        }
+        else if (boostInput <= 0)
+        {
+            playerSpeed = normalSpeed;
+        }
+    }
+
+    private void Forward()
+    {
+        float moveInput = moveAction.ReadValue<float>();
+        if (moveInput > 0)
+        {
+            Vector3 moveDirection = transform.forward * moveInput * playerSpeed;
+            rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z); 
+        }
     }
 }
