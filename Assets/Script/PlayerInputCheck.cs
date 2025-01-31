16df335b01d59e46aabb450a147a68a8951bb6ad
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-01-31 16:35:41 +0100

    stuff

---
+++ Assets/Script/PlayerInputCheck.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputCheck : MonoBehaviour
+{
+    
+    public bool forward;
+    public bool left;
+    public bool right;
+    public bool boost;
+    public bool drift;
+    
+    [SerializeField]
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction boostAction;
+    private InputAction turnAction;
+    private InputAction driftAction;
+
+    void Start()
+    {
+        moveAction = playerInput.actions.FindAction("Forward");
+        boostAction = playerInput.actions.FindAction("Boost");
+        turnAction = playerInput.actions.FindAction("Turn");
+        driftAction = playerInput.actions.FindAction("Drift");
+    }
+    void Update()
+    {
+        ForwardCheck();
+        TurnCheck();
+        BoostCheck();
+        DriftCheck();
+    }
+
+    private void ForwardCheck()
+    {
+        float moveInput = moveAction.ReadValue<float>();
+        switch (moveInput)
+        {
+            case 1f:
+            {
+                forward = true;
+                break;
+            }
+            case 0f:
+            {
+                forward = false;
+                break;
+            }
+        }
+    }
+
+    private void TurnCheck()
+    {
+        float turnInput = turnAction.ReadValue<float>();
+        switch (turnInput)
+        {
+            case 1f:
+            {
+                left = true;
+                right = false;
+                break;
+            }
+            case 0f:
+            {
+                left = false;
+                right = false;
+                break;
+            }
+            case -1f:
+            {
+                left = false;
+                right = true;
+                break;
+            }
+        }
+    }
+
+    private void BoostCheck()
+    {
+        float boostInput = boostAction.ReadValue<float>();
+        switch (boostInput)
+        {
+            case 1f:
+            {
+                boost = true;
+                break;
+            }
+            case 0f:
+            {
+                boost = false;
+                break;
+            }
+        }
+    }
+
+    private void DriftCheck()
+    {
+        float driftInput = driftAction.ReadValue<float>();
+        switch (driftInput)
+        {
+            case 1f:
+            {
+                drift = true;
+                break;
+            }
+            case 0f:
+            {
+                drift = false;
+                break;
+            }
+        }
+    }
+}
