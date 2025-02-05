2dca765e9914ce042051b7e52f95158d99a7ba1e
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-02-05 21:16:08 +0100

    trying stuff

---
+++ Assets/Script/Acceleration.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Acceleration : PlayerController
+{
+    public void NormalAcceleration()
+    {
+        
+    }
+
+    public void BoostAcceleration()
+    {
+        
+    }
+    
+    
+}
