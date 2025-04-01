6d9e1241d7385b225615b8ee39c832c1eb474596
Juji.cs <61978075+Jujics@users.noreply.github.com>
2025-04-01 09:33:41 +0200

    gamemanager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System;
+using Unity.VisualScripting;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager instance{get; private set;}
+
+    public enum GameState
+    {
+        Menu,
+        BeforeGame,
+        Dialoguelvl1,
+        Duringlvl1,
+        Dialoguelvl2,
+        Duringlvl2,
+        Dialoguelvl3,
+        Duringlvl3,
+        Dialoguelvl4,
+        Duringlvl4,
+        PostGame,
+        EndGame
+    }
+    public GameState CurrentState { get; private set; } 
+
+    public void SwitchState(GameState newState)  
+    {
+        CurrentState = newState;
+        Debug.Log($"Game state changed to: {newState}"); 
+
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void HandleState()
+    {
+        switch(CurrentState)
+        {
+            case GameState.Menu:
+                break;
+            case GameState.BeforeGame:
+                break;
+            case GameState.Dialoguelvl1:
+                break;
+            case GameState.Duringlvl1:
+                break;
+            case GameState.Dialoguelvl2:
+                break;
+            case GameState.Duringlvl2:
+                break;
+            case GameState.Dialoguelvl3:
+                break;
+            case GameState.Duringlvl3:
+                break;
+            case GameState.Dialoguelvl4:
+                break;
+            case GameState.Duringlvl4:
+                break;
+            case GameState.PostGame:
+                break;
+            case GameState.EndGame:
+                break;
+        }
+    }
+
+    void Start()
+    {
+        CurrentState = GameState.Menu;
+    }
+
+    void Update()
+    {
+        
+    }
+}
